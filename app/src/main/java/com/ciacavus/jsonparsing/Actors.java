cf0599d8a688a0ed3252436f5017cf0db09ae91c
CiaranMc93 <C11354741@mydit.ie>
2016-05-30 07:42:59 +0100

    Adding JSON parsing logic(Need to resolve HTTP errors)

--- app/src/main/java/com/ciacavus/jsonparsing/Actors.java
+++ app/src/main/java/com/ciacavus/jsonparsing/Actors.java
@@ -13,4 +13,102 @@ public class Actors {
     private String spouse;
     private String children;
     private String image;
+
+    public Actors(String name, String desc,String dob,
+                  String country,String height,String spouse,String children,String image){
+        super();
+
+        this.name = name;
+        this.desc = desc;
+        this.dob = dob;
+        this.country = country;
+        this.height = height;
+        this.spouse = spouse;
+        this.children = children;
+        this.image = image;
+
+
+
+    }
+
+    //individual function for getting and setting values for each information bit
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDesc()
+    {
+        return name;
+    }
+
+    public void setDesc(String desc)
+    {
+        this.desc = desc;
+    }
+
+    public String getDob()
+    {
+        return dob;
+    }
+
+    public void setDob(String dob)
+    {
+        this.dob = dob;
+    }
+
+    public String getCountry()
+    {
+        return country;
+    }
+
+    public void setCountry(String country)
+    {
+        this.country = country;
+    }
+
+    public String getHeight()
+    {
+        return height;
+    }
+
+    public void setHeight(String height)
+    {
+        this.height = height;
+    }
+
+    public String getSpouse()
+    {
+        return spouse;
+    }
+
+    public void setSpouse(String spouse)
+    {
+        this.spouse = spouse;
+    }
+
+    public String getChildren()
+    {
+        return children;
+    }
+
+    public void setChildren(String children)
+    {
+        this.children = children;
+    }
+
+    public String getImage()
+    {
+        return image;
+    }
+
+    public void setImage(String image)
+    {
+        this.image = image;
+    }
 }
--- app/src/main/java/com/ciacavus/jsonparsing/MainActivity.java
+++ app/src/main/java/com/ciacavus/jsonparsing/MainActivity.java
@@ -1,5 +1,7 @@
 package com.ciacavus.jsonparsing;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,7 +9,11 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.concurrent.Exchanger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         //create new actors array
         listItem = new ArrayList<Actors>();
 
+        new JSONAsyncTask().execute("http://microblogging.wingity.com/JSONParsingTutorial/jonActors");
+
         //link XML with Java Code
         ListView lv = (ListView)findViewById(R.id.list);
 
@@ -36,5 +44,78 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),listItem.get(position).getName(), Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    class JSONAsyncTask extends AsyncTask<String, Void, Boolean>{
+
+        ProgressDialog pd;
+
+        protected void OnPreExecute()
+        {
+            super.onPreExecute();
+
+            //performing logic before execution of thread
+            pd = new ProgressDialog(MainActivity.this);
+            pd.setMessage("Please Wait");
+            pd.setTitle("Connecting...");
+            pd.show();
+            pd.setCancelable(false);
+        }
+
+        //perform in the background
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            try {
+                //perform a HTTP request to get the data
+                HttpGet httppost = new HttpGet(urls[0]);
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpResponse response = httpclient.execute(httppost);
+
+                int statu = response.getStatuLine().getStatuCode();
+
+                //if the status returned is successful
+                if(status == 200)
+                {
+                    HttpEntity entity = response.getEntity();
+                    String data = EntityUtils.toString(entity);
+
+                    JSONObject jsonObj = new JSONObject(data);
+                    JSONArray jsonArr = jsonObj.getJSONArray("actors");
+
+                    //for each entity in the array list, populate the data
+                    for(int i = 0; i < jsonArr.length(); i++)
+                    {
+                        //get an object from the corresponding array value in the JSON Array
+                        JSONObject obj = jsonArr.getJSONObject(i);
+
+                        //create a new actor
+                        Actors actor = new Actors();
+
+                        actor.setName(obj.getString("name"));
+                        actor.setDesc(obj.getString("description"));
+                        actor.setDob(obj.getString("dob"));
+                        actor.setCountry(obj.getString("country"));
+                        actor.setHeight(obj.getString("height"));
+                        actor.setSpouse(obj.getString("spouse"));
+                        actor.setChildren(obj.getString("children"));
+                        actor.setImage(obj.getString("image"));
+
+                        //add actors to the list
+                        listItem.add(actor);
+
+                    }
+
+                    return true;
+                }
+
+            }
+            catch(Exception e)
+            {
+
+            }
+
+            return null;
+        }
     }
 }
